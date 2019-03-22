f65942174fc0f1b7ca401ddcde2790dd7587aa32
Avishay Alon <avishaya@amazon.com>
2019-03-22 22:06:28 +0000

    Refactoring pixy_node.py

--- pixy_node/src/pixy_node/pixy_node.py
+++ pixy_node/src/pixy_node/pixy_node.py
@@ -1,14 +1,15 @@
 import rospy
-from std_msgs.msg import String
+import math
+from std_msgs.msg import Int16
 from pixy import *
 from ctypes import *
 
-# Pixy Python SWIG get blocks example #
-
-print ("Pixy Python SWIG Example -- Get Blocks")
-
-# Initialize Pixy Interpreter thread #
-pixy_init()
+def get_angle(targetX, targetY):
+    # tweak orig if its not zero zero
+    origX = 0
+    origX = 0
+    myradians = math.atan2(targetY-origX, targetX-origX)
+    return math.degrees(myradians)
 
 class Blocks (Structure):
   _fields_ = [ ("type", c_uint),
@@ -19,19 +20,24 @@ class Blocks (Structure):
                ("height", c_uint),
                ("angle", c_uint) ]
 
-blocks = BlockArray(100)
-frame  = 0
+def _node_setup():
+    pixy_init()
+    pub = rospy.Publisher('pixy_angle', Int16, queue_size=10)
+    rospy.init_node('pixy_node', anonymous=True)
+    #rate = rospy.Rate(10) # 10hz
+    return pub
 
 def pixy_node():
-    count = pixy_get_blocks(100, blocks)
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('pixy_node', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    angle_publisher = _node_setup()
+    print 'Calling pixy_get_blocks'
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+        blocks = BlockArray(100)
+        count = pixy_get_blocks(100, blocks)
+        for index in range (0, count):
+            block_log = '[BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d ANGLE=%3d]' % (blocks[index].type, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height, blocks[index].angle)
+            print block_log
+            rospy.loginfo(block_log)
+            angle_publisher.publish(blocks[index].angle)
 
 if __name__ == '__main__':
     try:
